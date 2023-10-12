eb9e2be1fb488a4446207bb4b1d285d8405529fa
cooi0004 <cooi0004@student.monash.edu>
2023-10-12 13:10:11 +0000

    comm_func

--- assignment2/charging_node.h
+++ assignment2/charging_node.h
@@ -3,6 +3,12 @@
 int charging_nodes_func(int size, int rank, int base, int ndims, MPI_Comm master_comm, MPI_Comm comm);
 int initialise_charging_grid(int size, int rank, int ndims, int *dims, MPI_Comm existing_comm, MPI_Comm *new_comm);
 int incoming_car_simulator(int rank);
+enum MessageTags
+{
+    REQUEST_TAG,
+    RESPONSE_TAG,
+    TERMINATE_TAG
+};
 struct
 {
     int second;
--- assignment2/charging_nodes.c
+++ assignment2/charging_nodes.c
@@ -9,18 +9,24 @@
 #define SHIFT_ROW 0
 #define SHIFT_COL 1
 #define DISPLACEMENT 1
+#define MAX_NEIGHBOURS 4
 
 int shared_availability_counter = 0;
 charging_node_logs *charging_logs;
 pthread_mutex_t availabiltiy_counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t charging_logs_mutex = PTHREAD_MUTEX_INITIALIZER;
-int terminate_flag = 0;
+pthread_mutex_t termiate_flag_mutex = PTHREAD_MUTEX_INITIALIZER;
+volatile int terminate_flag = 0;
+volatile int communication_thread_done = 0;
+int shared_ndims;
+int shared_rank;
+MPI_Comm shared_comm;
 
 int initialise_charging_grid(int size, int rank, int ndims, int *dims, MPI_Comm existing_comm, MPI_Comm *new_comm)
 {
 
-    int periods[ndims]; // Non-periodic grid
-    int reorder = 0;    // preserve original ordering
+    int periods[2] = {0, 0}; // Non-periodic grid
+    int reorder = 0;         // preserve original ordering
 
     if (rank == 0)
         printf("Comm Size: %d: Grid Dimension =[%d x %d] \n", size, dims[0], dims[1]);
@@ -45,7 +51,7 @@ void *thread_function(void *arg)
         // Lock value mutex, perform operation, unlock
         pthread_mutex_lock(&availabiltiy_counter_mutex);
         int operation = rand() % 2;
-        if (operation == 0)
+        if (operation == 0 && shared_availability_counter < K)
         {
             shared_availability_counter++;
         }
@@ -61,40 +67,51 @@ void *thread_function(void *arg)
 
 void *communicate_neighbour_thread_func(void *arg)
 {
-    // int nodeRowUp, nodeRowBot, nodeColLeft, nodeColRight;
-    // int coord[ndims];
-    // MPI_Request neighbour_req[4];
-    // MPI_Status neighbour_status[4];
-    // MPI_Cart_coords(comm, rank, ndims, coord);
+    int nodeRowUp, nodeRowBot, nodeColLeft, nodeColRight;
+    int coord[shared_ndims];
+    MPI_Cart_coords(shared_comm, shared_rank, shared_ndims, coord);
 
-    // // get neighbour nodes
-    // MPI_Cart_shift(comm, SHIFT_ROW, DISPLACEMENT, &nodeRowBot, &nodeRowUp);
-    // MPI_Cart_shift(comm, SHIFT_COL, DISPLACEMENT, &nodeColLeft, &nodeColRight);
-    // if (nodeRowUp != MPI_PROC_NULL)
-    // {
-    //     MPI_Isend(&val, 1, MPI_INT, nodeRowUp, 0, comm, &neighbour_req[0]);
-    // }
-    // if (nodeRowBot != MPI_PROC_NULL)
-    // {
-    //     MPI_Isend(&val, 1, MPI_INT, nodeRowBot, 0, comm, &neighbour_req[1]);
-    // }
-    // if (nodeColLeft != MPI_PROC_NULL)
-    // {
-    //     MPI_Isend(&val, 1, MPI_INT, nodeColLeft, 0, comm, &neighbour_req[2]);
-    // }
-    // if (nodeColRight != MPI_PROC_NULL)
-    // {
-    //     MPI_Isend(&val, 1, MPI_INT, nodeColRight, 0, comm, &neighbour_req[3]);
-    // }
-    // MPI_Waitall(4, &neighbour_req, &neighbour_status);
-    printf("communicating\n");
-    // printf("rank: %d. Coord: (%d, %d). Left: %d. Right: %d. Top: %d. Bottom: %d\n", rank, coord[0], coord[1], nodeColLeft, nodeColRight, nodeRowBot, nodeRowUp);
+    int neighbours = 0;
+    int temp_val = 1;
+
+    // get neighbour nodes
+    MPI_Cart_shift(shared_comm, SHIFT_ROW, DISPLACEMENT, &nodeRowBot, &nodeRowUp);
+    MPI_Cart_shift(shared_comm, SHIFT_COL, DISPLACEMENT, &nodeColLeft, &nodeColRight);
+    printf("rank: %d. Coord: (%d, %d). Left: %d. Right: %d. Top: %d. Bottom: %d\n", shared_rank, coord[0], coord[1], nodeColLeft, nodeColRight, nodeRowBot, nodeRowUp);
+
+    int all_neighbour_ranks[MAX_NEIGHBOURS] = {nodeRowBot, nodeRowUp, nodeColLeft, nodeColRight};
+    MPI_Request neighbour_send_req[MAX_NEIGHBOURS];
+    MPI_Request neighbour_recieve_req[MAX_NEIGHBOURS];
+
+    MPI_Status neighbour_status[MAX_NEIGHBOURS];
+    int recieved_availability[MAX_NEIGHBOURS];
+    int counter = 0;
+    for (int i = 0; i < MAX_NEIGHBOURS; i++)
+    {
+        int neighbour_rank = all_neighbour_ranks[i];
+        printf("rank %d sending request to %d\n", shared_rank, neighbour_rank);
+        MPI_Isend(&temp_val, 1, MPI_INT, neighbour_rank, RESPONSE_TAG, shared_comm, &neighbour_send_req[i]);
+        MPI_Irecv(&recieved_availability[i], 1, MPI_INT, neighbour_rank, RESPONSE_TAG, shared_comm, &neighbour_recieve_req[i]);
+    }
+
+    MPI_Waitall(MAX_NEIGHBOURS, &neighbour_recieve_req, &neighbour_status);
+    for (int i = 0; i < MAX_NEIGHBOURS; i++)
+    {
+        printf("rank %d recieved %d from neighbour %d\n", shared_rank, recieved_availability[i], all_neighbour_ranks[i]);
+    }
+    pthread_mutex_lock(&termiate_flag_mutex);
+    communication_thread_done = 1;
+    pthread_mutex_unlock(&termiate_flag_mutex);
+    printf("done communicating\n");
 }
 
 int charging_nodes_func(int size, int rank, int base, int ndims, MPI_Comm master_comm, MPI_Comm comm)
 {
-    MPI_Status status;
-    MPI_Request req;
+    MPI_Status base_status, neighbour_status;
+    MPI_Request base_req, neighbour_req;
+    shared_rank = rank;
+    shared_ndims = ndims;
+    shared_comm = comm;
 
     int my_coords[ndims];
     const int k = 3;
@@ -117,12 +134,16 @@ int charging_nodes_func(int size, int rank, int base, int ndims, MPI_Comm master
     {
         pthread_create(&threads[i], NULL, thread_function, (void *)i);
     }
-    int received_value;
-    MPI_Irecv(&received_value, 1, MPI_INT, base, 0, master_comm, &req);
-
+    int received_value, temp_val = 0, comm_flag = 0;
+    MPI_Irecv(&received_value, 1, MPI_INT, base, MPI_ANY_TAG, master_comm, &base_req);
+    time_t now = time(NULL);
+    struct tm *ptm;
     while (1)
     {
-        MPI_Test(&req, &flag, &status);
+        now = time(NULL);
+        ptm = gmtime(&now);
+
+        MPI_Test(&base_req, &flag, &base_status);
         if (received_value == TERMINATE_VALUE)
         {
             printf("Process %d received termination signal.\n", rank);
@@ -133,21 +154,47 @@ int charging_nodes_func(int size, int rank, int base, int ndims, MPI_Comm master
             }
             break;
         }
-
-        int val = incoming_car_simulator(rank);
-        if (val <= NODE_THRESHOLD)
+        // MPI_Test(&neighbour_req, &comm_flag, &neighbour_status);
+        MPI_Iprobe(MPI_ANY_SOURCE, RESPONSE_TAG, comm, &comm_flag, &neighbour_status);
+        if (comm_flag)
         {
-            pthread_create(&communication_thread, NULL, communicate_neighbour_thread_func, (void *)rank);
+            MPI_Recv(&temp_val, 1, MPI_INT, neighbour_status.MPI_SOURCE, RESPONSE_TAG, comm, &neighbour_status);
+            printf("rank %d received request from %d\n", rank, neighbour_status.MPI_SOURCE);
+            pthread_mutex_lock(&availabiltiy_counter_mutex);
+            MPI_Isend(&shared_availability_counter, 1, MPI_INT, neighbour_status.MPI_SOURCE, RESPONSE_TAG, comm, &neighbour_req);
+            pthread_mutex_unlock(&availabiltiy_counter_mutex);
+
+            // set listner again
+            MPI_Iprobe(MPI_ANY_SOURCE, RESPONSE_TAG, comm, &comm_flag, &neighbour_status);
+
+            comm_flag = 0;
         }
+        if (ptm->tm_sec % interval == 0)
+        {
 
-        pthread_mutex_lock(&charging_logs_mutex);
-        insert_new_charging_node(charging_logs, val);
-        printf("Printing logs from rank %d\n", rank);
-        print_charging_logs(charging_logs);
-        pthread_mutex_unlock(&charging_logs_mutex);
+            pthread_mutex_lock(&availabiltiy_counter_mutex);
+            int current_availability = shared_availability_counter;
+            pthread_mutex_unlock(&availabiltiy_counter_mutex);
+            if (current_availability <= NODE_THRESHOLD)
+            {
+                pthread_create(&communication_thread, NULL, communicate_neighbour_thread_func, (void *)rank);
+            }
 
-        sleep(interval);
-        pthread_join(communication_thread, NULL);
+            pthread_mutex_lock(&charging_logs_mutex);
+            insert_new_charging_node(charging_logs, current_availability);
+            printf("Printing logs from rank %d\n", rank);
+            print_charging_logs(charging_logs);
+            pthread_mutex_unlock(&charging_logs_mutex);
+        }
+        pthread_mutex_lock(&termiate_flag_mutex);
+
+        if (communication_thread_done)
+        {
+            printf("terminating communication thread\n");
+            pthread_join(communication_thread, NULL);
+            communication_thread_done = 0;
+        }
+        pthread_mutex_unlock(&termiate_flag_mutex);
     }
 }
 
